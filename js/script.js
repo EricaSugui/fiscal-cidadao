e853ccc444cf4223aa6b8b47b6fc7306012e8482
EricaSugui <ericasugui@gmail.com>
2020-07-08 21:40:48 -0300

    adicionado geolocation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
     var $CPF = $("#jsCpf");
     $CPF.mask("000.000.000-00", {reverse: true});
+
+    function chooseFile() {
+        $("#fileInput").click();
+     }
+
+     var map;
+            function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8
+            });
+            }
 });
+
